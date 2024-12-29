7c2714e6b8c34ccc2530ad682e13de4c8b02519a
CelestialCorp <CelestialCorpContact@gmail.com>
2024-12-29 20:06:55 +0100

    Update config.js

--- config.js
+++ config.js
@@ -39,9 +39,9 @@ module.exports = {
   nodes: [
     {
         name: "Server",
-        host: "vs.lavalink.rocks",
-        port: 443,
-        password: "horizxon.tech",
+        host: "lavalink01.techbyte.host"",
+        port: 2036,
+        password: "NAIGLAVA-dash.techbyte.host",
         reconnectTimeout: 5000,
         reconnectTries: Infinity,
         secure: true
